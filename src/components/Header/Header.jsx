3023f501ed22ef413cc2a49b584d19e5ebb18050
shamaz332 <shamazsaeed332@gmail.com>
2021-04-17 16:14:42 +0500

    Side bar ready

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,73 @@
+import "./Header.css";
+
+import { Avatar, IconButton } from "@material-ui/core";
+
+import AddIcon from "@material-ui/icons/Add";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import FlagIcon from "@material-ui/icons/Flag";
+import ForumIcon from "@material-ui/icons/Forum";
+import HomeIcon from "@material-ui/icons/Home";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/768px-Facebook_icon.svg.png"
+          alt=""
+        />
+
+        <div className="header__input">
+          <SearchIcon />
+          <input placeholder="Search Facebook" type="text" />
+        </div>
+      </div>
+
+      <div className="header__center">
+        <div className="header_option header__option__active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header_option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Shamaz</h4>
+        </div>
+
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./SIdebar.css";
+import SidebarRow from "./SidebarRow";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <SidebarRow src="" title="shamaz" />
+      <SidebarRow
+        Icon={LocalHospitalIcon}
+        title="COVID-19 Information Center"
+      />
+      <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+      <SidebarRow Icon={PeopleIcon} title="Friends" />
+      <SidebarRow Icon={ChatIcon} title="Messenger" />
+      <SidebarRow Icon={StorefrontIcon} title="MarketPlace" />
+      <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+      <SidebarRow Icon={ExpandMoreIcon} title="More" />
+    </div>
+  );
+};
+
+export default Sidebar;
